fc8f881b1cf874b57c714892c8c5220d1d0e3aba
fatherwolf <engleau13@gmail.com>
2026-01-18 17:59:08 -0500

    Security fix: bind Next.js server to localhost only to prevent external access

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.prepare().then(() => {
       console.error(err)
       process.exit(1)
     })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
+    .listen(port, '127.0.0.1', () => {
+      console.log(`> Ready on http://127.0.0.1:${port}`)
     })
 })
